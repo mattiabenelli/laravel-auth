bb67f3b1f31bd86fcd0c55006d5488a2d7a2ea11
mattiabenelli <benellimattia0@gmail.com>
2023-03-01 15:36:16 +0100

    collegamento route projectcontroller

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -26,7 +26,7 @@
     <div id="app">
 
         <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">BoolPress</a>
+            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">BoolProject</a>
             <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu">
                 <span class="navbar-toggler-icon"></span>
             </button>
