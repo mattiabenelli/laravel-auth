3d1d5ce97ee1fd9a66fe3f0f36de712d88d89b07
mattiabenelli <benellimattia0@gmail.com>
2023-03-01 15:09:06 +0100

    creazione controller,model,seeder,migration,faker

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\DashboardController as DashboardController;
+use App\Http\Controllers\Admin\ProjectController as ProjectController;
 
 /*
 |--------------------------------------------------------------------------
